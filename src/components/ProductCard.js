4552b150660eac083190ce3ea075b55b90877f32
Your Name <you@example.com>
2024-01-20 09:33:41 +0200

    Indigrating Context and Reducer

---
+++ src/components/ProductCard.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from 'react';
+import "./ProductCard.css"
+import {useCart} from "../context/CartContext";
+
+export const ProductCard = ({product}) => {
+    const {addToCart, removeFromCart, cartList} = useCart()
+    const {id, name, price, image} = product
+    const [isInCart, setIsInCart] = useState(false)
+
+    useEffect(() => {
+        const productIsInCart = cartList.find(cartItem => cartItem.id === id)
+        if (productIsInCart) {
+            setIsInCart(true);
+        } else {
+            setIsInCart(false)
+        }
+    }, [cartList, id]);
+
+    return (
+        <div className={"product"}>
+            <img src={image} alt={name}/>
+            <p className={"name"}>{name}</p>
+            <div className={"action"}>
+                <p>${price}</p>
+                {isInCart ? (<button className={"remove"} onClick={() => {
+                    removeFromCart(product)
+                }}>Remove</button>) : (<button onClick={() => {
+                    addToCart(product)
+                }}>Add to cart</button>)}
+            </div>
+        </div>
+    );
+};
+
